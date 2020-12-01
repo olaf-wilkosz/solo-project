f6687813b2252053dc20565c47bee5f603fba538
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-12-01 22:11:16 +0100

    Add function changing the hamburger position according to sidebar's state

--- js/script.js
+++ js/script.js
@@ -11,12 +11,25 @@ const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 
-hamburger.addEventListener('click', function() {
+hamburger.addEventListener('click', function () {
   sidebar.classList.toggle('is-active');
   overlay.classList.toggle('is-active');
+
+  const hamburgerStatus = document.getElementsByClassName('is-active');
+
+  if (window.screen.height <= 1023) {
+    if (hamburgerStatus.length == 0) {
+      hamburger.setAttribute('style', 'left: 0');
+    } else {
+      hamburger.setAttribute('style', 'left: 260px');
+    }
+  } else if (window.screen.height > 1024) {
+    hamburger.setAttribute('style', 'left: 128px');
+  }
 });
 
-overlay.addEventListener('click', function() {
+
+overlay.addEventListener('click', function () {
   sidebar.classList.remove('is-active');
   overlay.classList.remove('is-active');
 });

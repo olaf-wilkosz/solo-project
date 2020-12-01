796f5dacd59500e657dfa2e6816cde84e62f9bf8
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-12-01 11:15:52 +0100

    Upgrade RWD sidebar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-function toggleMenu(visible) {
-  document.querySelector('.side-nav').classList.toggle('show-regular', visible);
-}
+// function toggleMenu(visible) {
+//   document.querySelector('.side-nav').classList.toggle('show-regular', visible);
+// }
 
-document.querySelector('.hamburger').addEventListener('click', function (event) {
-  event.preventDefault();
-  toggleMenu();
+// document.querySelector('.hamburger').addEventListener('click', function (event) {
+//   event.preventDefault();
+//   toggleMenu();
+// });
+
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+
+hamburger.addEventListener('click', function() {
+  sidebar.classList.toggle('is-active');
+  overlay.classList.toggle('is-active');
+});
+
+overlay.addEventListener('click', function() {
+  sidebar.classList.remove('is-active');
+  overlay.classList.remove('is-active');
 });

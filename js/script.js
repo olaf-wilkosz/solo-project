58b6ab83ca3ac2731b84adbb3265830f5cfda0d8
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-12-02 01:51:12 +0100

    Fix sidebar behaviour on click on overlay and update JS

--- js/script.js
+++ js/script.js
@@ -34,6 +34,22 @@ hamburger.addEventListener('click', function () {
 });
 
 overlay.addEventListener('click', function () {
-  sidebar.classList.remove('is-active');
-  overlay.classList.remove('is-active');
+  sidebar.classList.toggle('is-active');
+  overlay.classList.toggle('is-active');
+  bottombar.classList.toggle('is-active');
+
+  const hamburgerStatus = document.getElementsByClassName('is-active');
+
+  if (window.screen.height <= 1023) {
+    if (hamburgerStatus.length == 0) {
+      hamburger.setAttribute('style', 'left: 0');
+      bottombar.setAttribute('style', 'transform: translateX(-100%)');
+    } else {
+      hamburger.setAttribute('style', 'left: 260px');
+      bottombar.setAttribute('style', 'transform: translateX(0)');
+    }
+  } else if (window.screen.height > 1024) {
+    hamburger.setAttribute('style', 'left: 128px');
+    bottombar.setAttribute('style', 'display: none');
+  }
 });
